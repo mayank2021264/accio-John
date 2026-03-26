d96d5af62c11eab0472fc3e52899fbac21aea3a1
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-03-26 14:43:14 +0530

    Add files via upload

---
+++ John/f2_contest_template/script.js
@@ -0,0 +1,61 @@
+/** @format */
+
+let arr = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+
+function PrintDeveloperbyMap() {
+  //Write your code here , just console.log
+  arr.filter((element) => {
+    if(element.profession === "developer"){
+      console.log(element);
+    }
+  })
+}
+
+function PrintDeveloperbyForEach() {
+  //Write your code here , just console.log
+  arr.forEach((element) => {
+    if(element.profession === "developer"){
+      console.log(element);
+    }
+  })
+}
+
+function addData() {
+  //Write your code here, just console.log
+  let newEntry = {
+    id: 4,
+    name: "Neel",
+    age: "27",
+    profession: "Engineer"
+  }
+  arr.push(newEntry);
+  for(let element of arr){
+    console.log(element);
+  }
+}
+
+function removeAdmin() {
+  //Write your code here, just console.log
+  arr.filter((element) => {
+    if(element.profession !== "admin"){
+      console.log(element);
+    }
+  })
+}
+
+function concatenateArray() {
+  //Write your code here, just console.log
+  let arr2 = [
+    {id : 5, Name : "Kumar", Age : 24, Profession : "admin"},
+    {id : 6, Name : "Lokesh", Age : 28, Profession : "manager"},
+    {id : 7, Name : "Ravi", Age : 28, Profession : "developer"}
+  ];
+  let result = arr.concat(arr2);
+  for(let element of result){
+    console.log(element);
+  }
+}
